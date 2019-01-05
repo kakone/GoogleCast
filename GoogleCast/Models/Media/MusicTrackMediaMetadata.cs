5bb4b497b602a906fbc1a9cd47896063594cd8d7
Halil <hig-dev@users.noreply.github.com>
2019-01-05 21:42:11 +0100

    Create MusicTrackMediaMetadata.cs

---
+++ GoogleCast/Models/Media/MusicTrackMediaMetadata.cs
@@ -0,0 +1,44 @@
+using System.Runtime.Serialization;
+
+namespace GoogleCast.Models.Media
+{
+    /// <summary>
+    /// Music metadata
+    /// </summary>
+    [DataContract]
+    [KnownType(typeof(MusicTrackMediaMetadata))]
+    public class MusicTrackMediaMetadata : GenericMediaMetadata
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="MusicTrackMediaMetadata"/> class
+        /// </summary>
+        public MusicTrackMediaMetadata()
+        {
+            MetadataType = MetadataType.Music;
+        }
+
+        [DataMember(Name = "albumArtist", EmitDefaultValue = false)]
+        public string AlbumArtist { get; set; }
+
+        [DataMember(Name = "albumName", EmitDefaultValue = false)]
+        public string AlbumName { get; set; }
+
+        [DataMember(Name = "artist", EmitDefaultValue = false)]
+        public string Artist { get; set; }
+
+        [DataMember(Name = "composer", EmitDefaultValue = false)]
+        public string Composer { get; set; }
+
+        [DataMember(Name = "discNumber", EmitDefaultValue = false)]
+        public int? DiscNumber { get; set; }
+
+        [DataMember(Name = "releaseDate", EmitDefaultValue = false)]
+        public string ReleaseDate { get; set; }
+
+        [DataMember(Name = "songName", EmitDefaultValue = false)]
+        public string SongName { get; set; }
+
+        [DataMember(Name = "trackNumber", EmitDefaultValue = false)]
+        public int? TrackNumber { get; set; }
+    }
+}
