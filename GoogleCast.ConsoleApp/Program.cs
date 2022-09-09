886634f8fb57201fb538853b4b030726c93ea650
BeardInaSuit <BeardInaSuit@hotmail.com>
2022-09-08 21:25:41 -0400

    feat: ConsoleApp command line arguments

--- GoogleCast.ConsoleApp/Program.cs
+++ GoogleCast.ConsoleApp/Program.cs
@@ -1,48 +1,71 @@
 ﻿
 using System.Net;
+using CommandLine;
 using GoogleCast;
 using GoogleCast.Channels;
 using GoogleCast.Models.Cast;
 
-var sender = new Sender();
-var ip = "10.0.0.2";
 
-// Connect to the Chromecast
-Console.Write("Connection...");
-
-var connected = await sender.ConnectAsync(IPAddress.Parse(ip), 8009, 5000);
-if (connected)
+class ConsoleApp
 {
-    Console.WriteLine("Done.");
-
-    var alive = false;
-    var heartBeatChannel = sender.GetChannel<IHeartbeatChannel>();
-    heartBeatChannel.PingReceived += (object? sender, GoogleCast.Models.HeartBeat.PingEvent e) =>
+    public class Options
     {
-        alive = true;
-    };
+        public Options() { Host = string.Empty; }
 
-    Console.Write("Waiting for ChromeCast Feedback");
-    while (!alive)
-    {
-        await Task.Delay(1000);
-        Console.Write(".");
-
+        [Option('h', "host", Required = true, HelpText = "Host")]
+        public string Host { get; set; }
+        [Option('p', "port", Required = false, Default = 8009, HelpText = "Port")]
+        public int Port { get; set; }
+        [Option('t', "timeout", Required = false, Default = 5000, HelpText = "Timeout")]
+        public int Timeout { get; set; }
     }
-    Console.WriteLine(" Done.");
 
-    // Launch the default media receiver application
-    Console.Write("Initializing Cast Channel...");
-    var castChannel = sender.GetChannel<ICastChannel>();
-    await sender.LaunchAsync(castChannel);
-    Console.WriteLine(" Done.");
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                Task.Run(async () =>
+                {
+                    var sender = new Sender();
+                    Console.Write("Connection...");
 
-    // Load an example website
-    Console.Write("Load URL http://www.example.com...");
-    await castChannel.LoadUrl(new CastInformation { Url = "https://www.example.com" });
-    Console.WriteLine(" Done.");
-}
-else
-{
-    Console.WriteLine("Unable to connect to " + ip);
+                    var connected = await sender.ConnectAsync(IPAddress.Parse(o.Host), o.Port, o.Timeout);
+                    if (!connected)
+                    {
+                        Console.WriteLine("Unable to connect to " + o.Host);
+                        return;
+                    }
+
+                    Console.WriteLine("Done.");
+
+                    var alive = false;
+                    var heartBeatChannel = sender.GetChannel<IHeartbeatChannel>();
+                    heartBeatChannel.PingReceived += (object? sender, GoogleCast.Models.HeartBeat.PingEvent e) =>
+                    {
+                        alive = true;
+                    };
+
+                    Console.Write("Waiting for ChromeCast Feedback");
+                    while (!alive)
+                    {
+                        await Task.Delay(1000);
+                        Console.Write(".");
+
+                    }
+                    Console.WriteLine(" Done.");
+
+                    // Launch the default media receiver application
+                    Console.Write("Initializing Cast Channel...");
+                    var castChannel = sender.GetChannel<ICastChannel>();
+                    await sender.LaunchAsync(castChannel);
+                    Console.WriteLine(" Done.");
+
+                    // Load an example website
+                    Console.Write("Load URL http://www.example.com...");
+                    await castChannel.LoadUrl(new CastInformation { Url = "https://www.example.com" });
+                    Console.WriteLine(" Done.");
+                }).GetAwaiter().GetResult();
+            });
+    }
 }
